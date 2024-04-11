9a34f14a6223381ccb337688f39c882c4916f56b
Sean Leong <seanleong888@gmail.com>
2024-04-11 02:05:10 +0100

    feat: routes swagger doc

--- backend-PEM/index.js
+++ backend-PEM/index.js
@@ -18,18 +18,47 @@ import configurePassport from "./config/passport.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./utils/swagger-output.json" assert { type: "json" };
 import cors from "cors";
-
-// connectDB();
+import swaggerJSDoc from "swagger-jsdoc";
 
 const app = express();
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-// app.use('/api/v1', router);
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Express API for JSONPlaceholder",
+    version: "1.0.0",
+    description:
+      "This is a REST API application made with Express. It retrieves data from JSONPlaceholder.",
+    license: {
+      name: "Licensed Under MIT",
+      url: "https://spdx.org/licenses/MIT.html",
+    },
+    contact: {
+      name: "JSONPlaceholder",
+      url: "https://jsonplaceholder.typicode.com",
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:8136",
+      description: "Development server",
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
 
 app.use(cors());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
 
 app.use(
   session({
@@ -43,14 +72,6 @@ app.use(passport.session());
 
 configurePassport(passport);
 
-// app.use(
-//   "/api/users",
-//   userRoutes
-//   /*
-//   #swagger.tags = ['User']
-// */
-// );
-
 app.use("/api/blockContent", blockContentRoutes);
 app.use("/api/blockPage", blockPageRoutes);
 app.use("/api/groupContent", groupContentRoutes);
@@ -61,14 +82,3 @@ app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
-
-// - **POST /api/users - Register a user
-// - **POST /api/users/auth - Authenticate a user and get token
-// - **POST /api/users/logout - Logout user and clear cookie
-// - **GET /api/users/profile - Get user profile
-// - **PUT /api/users/progile - Update profile
-
-// Why the dependency
-
-// Test: Demo
-// Integration, unit test
--- backend-PEM/routes/blockContentRoutes.js
+++ backend-PEM/routes/blockContentRoutes.js
@@ -3,6 +3,109 @@ import { blockGroups } from "../controllers/blockContentController.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      videoDetails:
+ *          type: object
+ *          properties:
+ *              video_name:
+ *                  type: string
+ *                  description: Video name
+ *                  example: 2022 Test Video
+ *              video_url:
+ *                  type: string
+ *                  description: Video url
+ *                  example: 2022 Test Video Url
+ *
+ *      blockList:
+ *          type: array
+ *          items:
+ *              type: object
+ *              properties:
+ *                  title:
+ *                      type: string
+ *                      description: Block title
+ *                      example: "1: Mood"
+ *                  content:
+ *                      type: array
+ *                      items:
+ *                          type: string
+ *                          description: Block Content
+ *                          example: Mood Measurement Questionnaires
+ *                  random:
+ *                      type: boolean
+ *                      description: Block Randomization
+ *                      example: false
+ */
+
+/**
+ * @swagger
+ * /api/blockContent:
+ *  get:
+ *      summary: Retrieve a list of block contents
+ *      description: Retrieve a list of block contents
+ *      responses:
+ *          200:
+ *              description: A list of block contents
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              blockGroupsList:
+ *                                  type: array
+ *                                  items:
+ *                                      type: object
+ *                                      properties:
+ *                                          title:
+ *                                              type: string
+ *                                              description: The block title
+ *                                              example: 2022_test
+ *                                          originalTitle:
+ *                                              type: string
+ *                                              description: The block title
+ *                                              example: 2022_test
+ *                                          description:
+ *                                              type: string
+ *                                              description: The block description
+ *                                              example: Full 2022 test
+ *                                          recording:
+ *                                              type: boolean
+ *                                              description: Recording
+ *                                              example: false
+ *                                          videoDetails:
+ *                                              type: object
+ *                                              properties:
+ *                                                  video_name:
+ *                                                      type: string
+ *                                                      description: Video name
+ *                                                      example: 2022 Test Video
+ *                                                  video_url:
+ *                                                      type: string
+ *                                                      description: Video url
+ *                                                      example: 2022 Test Video Url
+ *                                          blockList:
+ *                                              type: array
+ *                                              items:
+ *                                                  type: object
+ *                                                  properties:
+ *                                                      title:
+ *                                                          type: string
+ *                                                          description: Block title
+ *                                                          example: "1: Mood"
+ *                                                      content:
+ *                                                          type: array
+ *                                                          items:
+ *                                                              type: string
+ *                                                              description: Block Content
+ *                                                              example: Mood Measurement Questionnaires
+ *                                                      random:
+ *                                                          type: boolean
+ *                                                          description: Block Randomization
+ *                                                          example: false
+ */
 router.get("/", blockGroups);
 
 export default router;
